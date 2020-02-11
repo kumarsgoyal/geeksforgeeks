f1baa5edf3b4e9b8d50428b15fa4513c5be8f3e8
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-02-11 12:40:52 +0530

    Create Detect Loop in linked list.cpp

---
+++ Linked List/Detect Loop in linked list.cpp
@@ -0,0 +1,16 @@
+int detectloop(Node *head) {
+    
+    // your code here
+    Node *slow = head;
+    Node *fast = head;
+    while(fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+        
+        if(slow == fast) {
+            return 1;
+        }
+    }
+    return 0;
+    
+}
