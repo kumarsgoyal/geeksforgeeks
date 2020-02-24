caea76b016ebdfc69271bc0c0022f0a5605bf721
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-02-24 18:51:38 +0530

    Update Next larger element.cpp

--- Stack/Next larger element.cpp
+++ Stack/Next larger element.cpp
@@ -44,3 +44,58 @@ int main() {
 	}
 	return 0;
 }
+
+
+
+// Another approach
+/*
+
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+void nextLargestElement(unsigned long long *arr, int size) {
+    stack<unsigned long long> stk;
+    long long *res = new long long[size];
+    
+    for(int i = size-1; i >= 0; i--) {
+        while(!stk.empty() && stk.top() <= arr[i]) {
+            stk.pop();
+        }
+        if(stk.empty()) {
+            res[i] = -1;
+        }
+        else {
+            res[i] = stk.top();
+        }
+        stk.push(arr[i]);
+    }
+    
+    for(int i = 0; i < size; i++) {
+        cout<<res[i]<<" ";
+    }
+    cout<<"\n";
+    free(res);
+    
+}
+
+
+int main() {
+	int t = 0;
+	cin >> t;
+	while(t--) {
+	    int size = 0;
+	    cin>>size;
+	    unsigned long long *arr = new unsigned long long[size];
+	    for(int i = 0; i < size; i++) {
+	        unsigned long long num;
+	        cin >> arr[i];
+	    }
+	    
+        nextLargestElement(arr, size);
+        free(arr);
+	}
+	return 0;
+}
+
+*/
