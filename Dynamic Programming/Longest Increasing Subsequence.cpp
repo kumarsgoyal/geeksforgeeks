311b65955a150055b08babc3812796c8c0163743
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-01 10:18:28 +0530

    Create Longest Increasing Subsequence.cpp

---
+++ Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int test_case = 0;
+	cin>>test_case;
+	
+	while(test_case--) {
+	    int size = 0;
+	    cin>>size;
+	    
+	    int *Arr = new int[size];
+	    int *temp = new int[size];
+	    for(int i = 0; i < size; i++) {
+	      cin>>Arr[i];
+	      temp[i] = 1;
+	    }
+	    
+	    for(int i = 1; i < size; i++) {
+	        for(int j = 0; j < i; j++) {
+	            if((Arr[j] < Arr[i])) {
+	                if(temp[i] == 1) {
+	                    temp[i] = temp[j] + 1;
+	                }
+	                else if(temp[i] <= temp[j]) {
+	                    temp[i] = temp[j]+1;
+	                }
+	            }
+	        }
+	    }
+	    int max = 0;
+	    for(int i = 0;i < size; i++) {
+	        if(temp[i] > max) {
+	            max = temp[i];
+	        }
+	    }
+	    
+	    
+	    
+	    cout<<max<<"\n";
+	}
+	return 0;
+}
