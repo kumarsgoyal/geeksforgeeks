a7199cd337cc3500e9e0ea1dfcbb50d6bc4312bd
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-07 12:40:00 +0530

    Update Height of Binary Tree.cpp

--- Tree/Height of Binary Tree.cpp
+++ Tree/Height of Binary Tree.cpp
@@ -1,95 +1,105 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-/* A binary tree node has data, pointer to left child
-   and a pointer to right child */
 struct Node
 {
     int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
+    struct Node *left;
+    struct Node *right;
 };
-
-
-/* Computes the number of nodes in a tree. */
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
 int height(struct Node* node);
 
-void inorder(Node *root)
-{
-    if (root == NULL)
-       return;
-    inorder(root->left);
-    cout << root->data << " ";
-    inorder(root->right);    
-}
-
-/* Driver program to test size function*/
 int main()
 {
-  int t;
-  scanf("%d\n", &t);
-  while (t--)
-  {
-     map<int, Node*> m;
-     int n;
-     scanf("%d",&n);
-     struct Node *root = NULL;
-     struct Node *child;
-     while (n--)
-     {
-        Node *parent;
-        char lr;
-        int n1, n2;
-        scanf("%d %d %c", &n1, &n2, &lr);
-
-        if (m.find(n1) == m.end())
-        {
-           parent = new Node(n1);
-           m[n1] = parent;
-           if (root == NULL)
-             root = parent;
-        }
-        else
-           parent = m[n1];
-
-        child = new Node(n2);
-        if (lr == 'L')
-          parent->left = child;
-        else
-          parent->right = child;
-        m[n2]  = child;
-     }
-
-     cout << height(root) << endl;
-  }
-  return 0;
-}
-/*This is a function problem.You only need to complete the function given below*/
-/* Tree node structure  used in the program
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+		cout<<height(root)<<endl;
     }
-};*/
-/* Computes the height of binary tree with given root.  */
-int max(int a, int b) {
-    return (a > b) ? a : b;
+    return 0;
 }
-int height(Node* node)
-{
-   // Your code here
-   if(node == NULL) {
-       return 0;
-   }
-   return (1 + max(height(node->left),height(node->right)));
+// } Driver Code End
+
+int height(Node *node) {
+    if(node == NULL) {
+        return 0;
+    }
+    
+    return max(height(node->left), height(node->right)) + 1;
 }
