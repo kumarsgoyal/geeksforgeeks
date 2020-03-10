1eec26d5e15bf739757a9011a09cf8084b53ea72
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-10 17:35:47 +0530

    Create Print BST elements in given range.cpp

---
+++ Tree/Print BST elements in given range.cpp
@@ -0,0 +1,98 @@
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+    
+    Node(int x){
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+
+Node *insert(Node *root,int data)
+{
+    if(root==NULL)
+    root=new Node(data);
+    else if(data<root->data)
+    root->left=insert(root->left,data);
+    else
+    root->right=insert(root->right,data);
+    
+    return root;
+}
+
+
+
+ // } Driver Code Ends
+/* The structure of a BST Node is as follows:
+
+struct Node {
+    int data;
+    Node* right;
+    Node* left;
+    
+    Node(int x){
+        data = x;
+        right = NULL;
+        left = NULL;
+    }
+};
+*/
+
+void printNearNodes(Node *root, int l, int h)
+{
+    if(root == NULL) {
+        return;
+    }
+    
+    if(l < root->data) {
+        printNearNodes(root->left, l, h);
+    }
+    
+    if(l <= root->data && root->data <= h) {
+        cout<<root->data<<" ";
+    }
+    
+    if ( h > root->data )  
+        printNearNodes(root->right, l, h);
+    
+    
+}
+
+
+// { Driver Code Starts.
+
+
+int main() {
+    
+    int testcases;
+    cin>>testcases;
+    while(testcases--)
+    {
+        Node *root=NULL;
+        int sizeOfArray;
+        cin>>sizeOfArray;
+        int arr[sizeOfArray];
+        
+        for(int i=0;i<sizeOfArray;i++)
+        cin>>arr[i];
+        
+        for(int i=0;i<sizeOfArray;i++)
+        {
+            root=insert(root,arr[i]);
+        }
+        int l,h;
+        cin>>l>>h;
+        printNearNodes(root,l,h);
+        cout<<endl;
+
+    }
+	return 0;
+}
+  // } Driver Code Ends
