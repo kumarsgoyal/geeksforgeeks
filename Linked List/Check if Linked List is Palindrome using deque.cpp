d4c27b70e0c04efc5713da07a7fad5ed932826b6
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-15 18:42:04 +0530

    Create Check if Linked List is Palindrome using deque.cpp

---
+++ Linked List/Check if Linked List is Palindrome using deque.cpp
@@ -0,0 +1,101 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+
+bool isPalindrome(Node *head);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+    cin>>T;
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+   	cout<<isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+/*This is a function problem.You only need to complete the function given below*/
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+/*You are required to complete this method */
+
+
+bool isPalindrome(Node *head)
+{
+    // Linked list is having only one node
+    if(head != NULL && head->next == NULL) {
+        return 1;
+    }
+    
+    else {
+        deque<int> dq;
+        
+        Node *curr = head;
+        int size = 0;
+        
+        
+        while(curr) {
+            size++;
+            dq.push_back(curr->data);
+            curr = curr->next;
+        }
+        
+        
+        while(size > 1) {
+            int front = dq.front();
+            int back = dq.back();
+            
+            dq.pop_back();
+            dq.pop_front();
+            
+            if(front != back) {
+                return 0;
+            }
+            
+            size -= 2;
+        }
+        
+        return 1;
+    }
+}
