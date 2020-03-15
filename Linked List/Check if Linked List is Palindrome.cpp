430c79bdb7067b4dc40daa587419f1d6dec5d975
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-15 09:32:03 +0530

    Update Check if Linked List is Palindrome.cpp

--- Linked List/Check if Linked List is Palindrome.cpp
+++ Linked List/Check if Linked List is Palindrome.cpp
@@ -1,3 +1,65 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <stack>
+using namespace std;
+/* Link list Node */
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+
+bool isPalindrome(Node *head);
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+    cin>>T;
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+   	cout<<isPalindrome(head)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+/*This is a function problem.You only need to complete the function given below*/
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+/*You are required to complete this method */
+
 void reverse(Node **head) {
     Node *curr = *head;
     Node *prev = NULL;
@@ -35,45 +97,57 @@ bool cmp(Node *head1, Node *head2) {
 
 bool isPalindrome(Node *head)
 {
-    //Your code here
-    if(head != NULL && head->next != NULL) {
-    bool res;
-    Node *slow = head;
-    Node *fast = head;
-    
-    Node *prev = NULL;
-    Node *second = NULL;
-    Node *mid = NULL;
-    
-    while(fast != NULL && fast->next != NULL) {
-        prev = slow;
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    
-    if(fast != NULL) {
-        mid = slow;
-        slow = slow->next;
-    }
-    
-    second = slow;
-    prev->next = NULL;
-    
-    reverse(&second);
-    
-    res =  cmp(head, second);
-    
-    reverse(&second);
-    
-    if(mid != NULL) {
-        prev->next = mid;
-        mid->next = second;
-    }
-    
-    
-    return res;
-    }
-    else {
+    // Linked list is having only one node
+    if(head != NULL && head->next == NULL) {
         return 1;
+    }
+    
+    else {
+        
+        bool res;
+        
+        // find middle element
+        Node *slow = head; // slow pointer
+        Node *fast = head; // fast pointer
+        Node *prev = NULL; 
+        
+        while(fast && fast->next) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        
+        
+        Node *mid = NULL;
+        // in case of odd element in list fast is not NULL
+        // in case of even element fast is NULL
+        // and we store mid element so that odd list is divided equally
+        if(fast != NULL) {
+            mid = slow;
+            slow = slow->next;
+        }
+        
+        // store head to second half
+        Node *second = NULL;
+        second = slow;
+        
+        // to terminate first half linked list
+        prev->next = NULL;
+        
+        reverse(&second);
+        
+        res =  cmp(head, second);
+        
+        reverse(&second);
+        
+        // join two list again
+        if(mid != NULL) {
+            prev->next = mid;
+            mid->next = second;
+        }
+        else {
+            prev->next = second;
+        }
+        return res;
     }
 }
