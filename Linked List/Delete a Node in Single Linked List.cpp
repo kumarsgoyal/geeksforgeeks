7b1a588d21b92a9c01e041aeba00d6a4e5ba7948
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-16 12:26:37 +0530

    Update Delete a Node in Single Linked List.cpp

--- Linked List/Delete a Node in Single Linked List.cpp
+++ Linked List/Delete a Node in Single Linked List.cpp
@@ -1,22 +1,106 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref,
+           int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+/* Function to get the middle of the linked list*/
+struct Node* deleteNode(struct Node *head,int );
+
+void printList(Node *head)
+{
+   while (head != NULL)
+   {
+      cout << head->data << " ";
+      head = head->next;
+   }
+   cout << "\n";
+}
+
+/* Driver program to test above function*/
+int main()
+{
+  int T, i, n, l;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+
+	int kk;
+	cin>>kk;
+    head = deleteNode(head,kk);
+    printList(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+/*This is a function problem.You only need to complete the function given below*/
+/* Link list Node 
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+/*You are required to complete below method*/
 Node* deleteNode(Node *head,int x)
 {
-    int pos = 1;
-    //Your code here
-    Node *temp = head;
-    Node *prev = NULL;
-    if(x == 1)
-    {
+    if(x == 1) {
         return head->next;
+    } 
+    
+    Node *curr = head;
+    Node *prev = NULL;
+    int pos = 1;
+    
+    while(curr && pos < x) {
+        pos++;
+        prev = curr;
+        curr = curr->next;
     }
-    while(pos != x)
-    {
-        ++pos;
-        prev = head;
-        head = head->next;
+    
+    if(curr) {
+        prev->next = curr->next;
     }
-    if(head != NULL)
-    {
-        prev->next = head->next;
-    }
-    return temp;
+    
+    return head;
+    
 }
