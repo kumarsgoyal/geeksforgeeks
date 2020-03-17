a4f50bf6cbb76a415c39455caf9f94e971ecd01a
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-17 11:10:41 +0530

    Create Doubly linked list Insertion at given position.cpp

---
+++ Linked List/Doubly linked list Insertion at given position.cpp
@@ -0,0 +1,110 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* a Node of the doubly linked list */
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x)
+  {
+      data = x;
+      next = prev = NULL;
+  }
+};
+
+void addNode(Node *head, int pos, int data);
+
+Node *insert(Node *head, int x)
+{
+    if (head == NULL)
+    {
+        return new Node(x);
+    }
+    Node *curr;
+    for (curr = head; curr->next != NULL; curr = curr->next)
+      ;
+    Node *temp = new Node(x);
+    curr->next = temp;
+    temp->prev = curr;
+    return head;
+}
+
+void printList(Node *head)
+{
+  // The purpose of below two loops is
+  // to test the created DLL
+  Node *temp=head;
+  if (temp != NULL) {
+ 
+  while (temp->next!=NULL)
+    temp=temp->next;
+  while (temp->prev!=NULL)
+   temp = temp->prev;
+  }
+  while (temp != NULL)
+  {
+      printf("%d ",temp->data);
+      temp=temp->next;
+  }
+  
+  cout << endl;
+}
+
+int main()
+{
+  int t;
+  scanf("%d",&t);
+  while(t--)
+  {
+  Node *head = NULL;   
+  int n;
+  scanf("%d",&n);
+  for(int i=0;i<n;i++){
+     int x;
+     scanf("%d",&x);
+     head = insert(head, x);
+  }     
+  int pos,data;
+  cin>>pos>>data;
+  addNode(head, pos, data);
+  printList(head);
+  }
+  return 0;
+}// } Driver Code Ends
+/* a Node of the doubly linked list 
+struct Node
+{
+  int data;
+  struct Node *next;
+  struct Node *prev;
+  Node(int x) { data = x; next = prev = NULL; }
+}; */
+
+/* Function to insert into a Doubly Linked List */
+void addNode(Node *head, int pos, int data)
+{
+    Node *newData = new Node(data);
+    Node *curr = head;   
+    if(head == NULL) {
+       head = newData;
+       return;
+    }
+    
+    for(int i = 0; i < pos; i++) {
+        curr = curr->next;
+    }
+    
+    if(curr->next == NULL) {
+        curr->next = newData;
+        newData->prev = curr;
+    }
+    else {
+        newData->prev = curr;
+        newData->next = curr->next;
+        curr->next = newData;
+        newData->next->prev = newData;
+    }
+}
