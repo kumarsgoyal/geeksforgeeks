e236682251fbe4c2ad3215b0e5d8100b03b51436
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-17 18:18:25 +0530

    Create Reorder List by dividing list into 2.cpp

---
+++ Linked List/Reorder List by dividing list into 2.cpp
@@ -0,0 +1,141 @@
+// { Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+#include<bits/stdc++.h>
+using namespace std;
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void reorderList(struct Node* head);
+
+/* Function to create a new Node with given data */
+struct Node* newNode(int data) {
+    struct Node* new_Node = new Node(data);
+    new_Node->data = data;
+    new_Node->next = NULL;
+    return new_Node;
+}
+
+void printList(struct Node* Node) {
+    while (Node != NULL) {
+        printf("%d ", Node->data);
+        Node = Node->next;
+    }
+    printf("\n");
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(void) {
+    int t, n, m, i, x;
+
+    cin >> t;
+    while (t--) {
+        struct Node* temp, *head;
+        cin >> n;
+        cin >> x;
+        head = new Node(x);
+        temp = head;
+        for (i = 0; i < n - 1; i++) {
+            cin >> x;
+            temp->next = new Node(x);
+            temp = temp->next;
+        }
+
+        reorderList(head);
+        printList(head);
+        freeList(head);
+    }
+    return 0;
+}
+// } Driver Code Ends
+/* Following is the Linked list node structure */
+
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+void reorderList(Node* head) {
+    
+    if(head == NULL) {
+        return;
+    }
+    
+    if(head != NULL && head->next == NULL) {
+        return;
+    }
+    
+    Node *slow = head;
+    Node *fast = head;
+    Node *prev = NULL;
+    Node *first = head;
+    Node *second = head;
+    
+    while(fast && fast->next) {
+        prev = slow;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    if(fast) {
+        prev = prev->next;
+        slow = slow->next;
+    }
+    
+    prev->next = NULL;
+    
+    second = slow;
+    
+    prev = NULL;
+    Node *next = NULL;
+    
+    while(second) {
+        next = second->next;
+        second->next = prev;
+        prev = second;
+        second = next;
+    }
+    
+    second = prev;
+    
+    prev = NULL;
+    while(second || first) {
+        
+        next = first->next;
+        if(prev != NULL) {
+            prev->next = first;
+        }
+        if(second) {
+            first->next = second;
+            prev = second;
+            second = second->next;
+        }
+        first = next;
+    }
+
+}
