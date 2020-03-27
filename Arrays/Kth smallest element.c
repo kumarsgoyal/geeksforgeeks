4d5d027d601c4f2824472cf71c1d2a9085081dca
Sahil Kumar <43093347+kumarsgoyal@users.noreply.github.com>
2020-03-27 13:26:16 +0530

    Create Kth smallest element.c

---
+++ Arrays/Kth smallest element.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int cmp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int main() {
+	//code
+	int t = 0;
+	scanf("%d", &t);
+	while(t--) {
+	    int size = 0;
+	    
+	    scanf("%d", &size);
+	    
+	    int *a = (int *)calloc(sizeof(int), size);
+	    
+	    for(int i = 0 ; i < size; i++) {
+	        scanf("%d", &a[i]);
+	    }
+	    
+	    int k = 0;
+	    scanf("%d", &k);
+	    qsort(a, size, sizeof(int), cmp);
+	    
+	    printf("%d", a[k - 1]);
+	    printf("\n");
+	}
+	return 0;
+}
